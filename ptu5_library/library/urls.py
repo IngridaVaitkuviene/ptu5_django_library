564e507d273fa79950748cd5ba3cccf405f8fd17
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-10 13:01:11 +0200

    authors, author, book_list,detail

--- ptu5_library/library/urls.py
+++ ptu5_library/library/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('authors/', views.authors, name='authors'),
+    path('author/<int:author_id>/', views.author, name='author'),
+    path('books/', views.BooklistView.as_view(), name='books'),
+    # pk - primary key
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book'),
 ]
