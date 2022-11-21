f1eff538897f0226d0352fceef8d0cf2601c1c60
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-21 10:34:42 +0200

    patikslinta datetime.utcnow()

--- ptu5_library/library/forms.py
+++ ptu5_library/library/forms.py
@@ -2,13 +2,12 @@ from django import forms
 from . models import BookReview
 from django.utils.timezone import datetime, timedelta
 
-
 class BookReviewForm(forms.ModelForm):
     def is_valid(self):
         valid = super().is_valid()
         if valid:
             reader = self.cleaned_data.get("reader")
-            recent_posts = BookReview.objects.filter(reader=reader, created_at__gte=(datetime.now() - timedelta(days=1)))
+            recent_posts = BookReview.objects.filter(reader=reader, created_at__gte=(datetime.utcnow() - timedelta(hours=1)))
             print(recent_posts)
             if recent_posts:
                 return False
