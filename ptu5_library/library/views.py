0f579abe8911f902a0c23e12dc94ab89645c5423
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-22 10:05:42 +0200

    user book instance create view

--- ptu5_library/library/views.py
+++ ptu5_library/library/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . models import Genre, Author, Book, BookInstance
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormMixin
 from .forms import BookReviewForm
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib import messages
 
-
-# Create your views here.
 
 def index(request):
     # return HttpResponse("Sveiki atvykę!")
@@ -111,3 +109,15 @@ class UserBookListView(LoginRequiredMixin, ListView):
         queryset = super().get_queryset()
         queryset = queryset.filter(reader=self.request.user).order_by('due_back')
         return queryset
+
+# klasese turim naudoti ne reverse, o reverse_lazy
+class UserBookInstanceCreateView(LoginRequiredMixin, CreateView):
+    model = BookInstance
+    fields = ('book', 'due_back', )
+    template_name = 'library/user_bookinstance_create.html'
+    success_url = reverse_lazy('user_books')
+
+    def form_valid(self, form):
+        form.instance.reader = self.request.user
+        form.instance.status = 'r'
+        return super().form_valid(form)
