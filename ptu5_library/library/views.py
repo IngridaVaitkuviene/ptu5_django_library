a8e35bb544e129d4c23388dd9ae701cf906cc569
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-22 13:25:45 +0200

    user book instance delete

--- ptu5_library/library/views.py
+++ ptu5_library/library/views.py
@@ -148,3 +148,20 @@ class UserBookInstanceUpdateView(LoginRequiredMixin, UserPassesTestMixin, Update
             context['action'] = 'Take'
         return context
         
+
+class UserBookInstanceDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = BookInstance
+    template_name = 'library/user_bookinstance_delete.html'
+    success_url = reverse_lazy('user_books')
+
+    def test_func(self):
+        book_instance = self.get_object()
+        return self.request.user == book_instance.reader
+
+    def form_valid(self, form):
+        book_instance = self.get_object()
+        if book_instance.status == 't':
+            messages.success(self.request, 'Book returned.')
+        else:
+            messages.success(self.request, 'Book reservation canceled.')
+        return super().form_valid(form)
